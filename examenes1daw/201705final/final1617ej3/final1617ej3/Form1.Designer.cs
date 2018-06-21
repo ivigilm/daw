c0a2c9712b1f0069fcef8e11e2c87d2b65d2decd
Isabel <isabel.vigil.moran@gmail.com>
2018-06-21 21:13:06 +0200

    Añadido el último ejercicio del examen de mayo de 2017

---
+++ examenes1daw/201705final/final1617ej3/final1617ej3/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace final1617ej3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTcontrasena = new System.Windows.Forms.Button();
+            this.BTnombre = new System.Windows.Forms.Button();
+            this.BTcancelar = new System.Windows.Forms.Button();
+            this.BTaceptar = new System.Windows.Forms.Button();
+            this.BTemail = new System.Windows.Forms.Button();
+            this.BTedad = new System.Windows.Forms.Button();
+            this.TBemail2 = new System.Windows.Forms.TextBox();
+            this.TBemail1 = new System.Windows.Forms.TextBox();
+            this.TBedad = new System.Windows.Forms.TextBox();
+            this.TBcontrasena2 = new System.Windows.Forms.TextBox();
+            this.TBcontrasena1 = new System.Windows.Forms.TextBox();
+            this.TBnombre = new System.Windows.Forms.TextBox();
+            this.LABemail2 = new System.Windows.Forms.Label();
+            this.LABemail1 = new System.Windows.Forms.Label();
+            this.LABedad = new System.Windows.Forms.Label();
+            this.LABcontrasena2 = new System.Windows.Forms.Label();
+            this.LABcontrasena1 = new System.Windows.Forms.Label();
+            this.LABnombre = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.colusuario = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colcontrasenia = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.coledad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colemail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // BTcontrasena
+            // 
+            this.BTcontrasena.Location = new System.Drawing.Point(232, 113);
+            this.BTcontrasena.Name = "BTcontrasena";
+            this.BTcontrasena.Size = new System.Drawing.Size(75, 83);
+            this.BTcontrasena.TabIndex = 4;
+            this.BTcontrasena.Text = "Validar";
+            this.BTcontrasena.UseVisualStyleBackColor = true;
+            this.BTcontrasena.Click += new System.EventHandler(this.BTcontrasena_Click);
+            // 
+            // BTnombre
+            // 
+            this.BTnombre.Location = new System.Drawing.Point(232, 44);
+            this.BTnombre.Name = "BTnombre";
+            this.BTnombre.Size = new System.Drawing.Size(75, 23);
+            this.BTnombre.TabIndex = 1;
+            this.BTnombre.Text = "Validar";
+            this.BTnombre.UseVisualStyleBackColor = true;
+            this.BTnombre.Click += new System.EventHandler(this.BTnombre_Click);
+            // 
+            // BTcancelar
+            // 
+            this.BTcancelar.Location = new System.Drawing.Point(362, 224);
+            this.BTcancelar.Name = "BTcancelar";
+            this.BTcancelar.Size = new System.Drawing.Size(89, 48);
+            this.BTcancelar.TabIndex = 11;
+            this.BTcancelar.Text = "Cancelar";
+            this.BTcancelar.UseVisualStyleBackColor = true;
+            this.BTcancelar.Click += new System.EventHandler(this.BTcancelar_Click);
+            // 
+            // BTaceptar
+            // 
+            this.BTaceptar.Location = new System.Drawing.Point(189, 224);
+            this.BTaceptar.Name = "BTaceptar";
+            this.BTaceptar.Size = new System.Drawing.Size(140, 48);
+            this.BTaceptar.TabIndex = 10;
+            this.BTaceptar.Text = "Añadir a la lista";
+            this.BTaceptar.UseVisualStyleBackColor = true;
+            this.BTaceptar.Click += new System.EventHandler(this.BTaceptar_Click);
+            // 
+            // BTemail
+            // 
+            this.BTemail.Location = new System.Drawing.Point(604, 113);
+            this.BTemail.Name = "BTemail";
+            this.BTemail.Size = new System.Drawing.Size(75, 83);
+            this.BTemail.TabIndex = 9;
+            this.BTemail.Text = "Validar";
+            this.BTemail.UseVisualStyleBackColor = true;
+            this.BTemail.Click += new System.EventHandler(this.BTemail_Click);
+            // 
+            // BTedad
+            // 
+            this.BTedad.Location = new System.Drawing.Point(604, 45);
+            this.BTedad.Name = "BTedad";
+            this.BTedad.Size = new System.Drawing.Size(75, 23);
+            this.BTedad.TabIndex = 6;
+            this.BTedad.Text = "Validar";
+            this.BTedad.UseVisualStyleBackColor = true;
+            this.BTedad.Click += new System.EventHandler(this.BTedad_Click);
+            // 
+            // TBemail2
+            // 
+            this.TBemail2.Location = new System.Drawing.Point(352, 174);
+            this.TBemail2.Name = "TBemail2";
+            this.TBemail2.Size = new System.Drawing.Size(236, 22);
+            this.TBemail2.TabIndex = 8;
+            // 
+            // TBemail1
+            // 
+            this.TBemail1.Location = new System.Drawing.Point(352, 113);
+            this.TBemail1.Name = "TBemail1";
+            this.TBemail1.Size = new System.Drawing.Size(236, 22);
+            this.TBemail1.TabIndex = 7;
+            // 
+            // TBedad
+            // 
+            this.TBedad.Location = new System.Drawing.Point(352, 45);
+            this.TBedad.Name = "TBedad";
+            this.TBedad.Size = new System.Drawing.Size(236, 22);
+            this.TBedad.TabIndex = 5;
+            // 
+            // TBcontrasena2
+            // 
+            this.TBcontrasena2.Location = new System.Drawing.Point(12, 174);
+            this.TBcontrasena2.Name = "TBcontrasena2";
+            this.TBcontrasena2.Size = new System.Drawing.Size(196, 22);
+            this.TBcontrasena2.TabIndex = 3;
+            // 
+            // TBcontrasena1
+            // 
+            this.TBcontrasena1.Location = new System.Drawing.Point(15, 113);
+            this.TBcontrasena1.Name = "TBcontrasena1";
+            this.TBcontrasena1.Size = new System.Drawing.Size(196, 22);
+            this.TBcontrasena1.TabIndex = 2;
+            // 
+            // TBnombre
+            // 
+            this.TBnombre.Location = new System.Drawing.Point(15, 44);
+            this.TBnombre.Name = "TBnombre";
+            this.TBnombre.Size = new System.Drawing.Size(196, 22);
+            this.TBnombre.TabIndex = 0;
+            // 
+            // LABemail2
+            // 
+            this.LABemail2.AutoSize = true;
+            this.LABemail2.Location = new System.Drawing.Point(359, 154);
+            this.LABemail2.Name = "LABemail2";
+            this.LABemail2.Size = new System.Drawing.Size(172, 17);
+            this.LABemail2.TabIndex = 12;
+            this.LABemail2.Text = "Repetir correo electrónico";
+            // 
+            // LABemail1
+            // 
+            this.LABemail1.AutoSize = true;
+            this.LABemail1.Location = new System.Drawing.Point(349, 93);
+            this.LABemail1.Name = "LABemail1";
+            this.LABemail1.Size = new System.Drawing.Size(124, 17);
+            this.LABemail1.TabIndex = 13;
+            this.LABemail1.Text = "Correo electrónico";
+            // 
+            // LABedad
+            // 
+            this.LABedad.AutoSize = true;
+            this.LABedad.Location = new System.Drawing.Point(349, 24);
+            this.LABedad.Name = "LABedad";
+            this.LABedad.Size = new System.Drawing.Size(41, 17);
+            this.LABedad.TabIndex = 14;
+            this.LABedad.Text = "Edad";
+            // 
+            // LABcontrasena2
+            // 
+            this.LABcontrasena2.AutoSize = true;
+            this.LABcontrasena2.Location = new System.Drawing.Point(12, 154);
+            this.LABcontrasena2.Name = "LABcontrasena2";
+            this.LABcontrasena2.Size = new System.Drawing.Size(129, 17);
+            this.LABcontrasena2.TabIndex = 15;
+            this.LABcontrasena2.Text = "Repetir contraseña";
+            // 
+            // LABcontrasena1
+            // 
+            this.LABcontrasena1.AutoSize = true;
+            this.LABcontrasena1.Location = new System.Drawing.Point(12, 93);
+            this.LABcontrasena1.Name = "LABcontrasena1";
+            this.LABcontrasena1.Size = new System.Drawing.Size(81, 17);
+            this.LABcontrasena1.TabIndex = 16;
+            this.LABcontrasena1.Text = "Contraseña";
+            // 
+            // LABnombre
+            // 
+            this.LABnombre.AutoSize = true;
+            this.LABnombre.Location = new System.Drawing.Point(12, 24);
+            this.LABnombre.Name = "LABnombre";
+            this.LABnombre.Size = new System.Drawing.Size(129, 17);
+            this.LABnombre.TabIndex = 17;
+            this.LABnombre.Text = "Nombre de usuario";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colusuario,
+            this.colcontrasenia,
+            this.coledad,
+            this.colemail});
+            this.listView1.Location = new System.Drawing.Point(12, 289);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(667, 149);
+            this.listView1.TabIndex = 18;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // colusuario
+            // 
+            this.colusuario.Text = "Usuario";
+            this.colusuario.Width = 150;
+            // 
+            // colcontrasenia
+            // 
+            this.colcontrasenia.Text = "Contraseña";
+            this.colcontrasenia.Width = 150;
+            // 
+            // coledad
+            // 
+            this.coledad.Text = "Edad";
+            this.coledad.Width = 100;
+            // 
+            // colemail
+            // 
+            this.colemail.Text = "Correo electrónico";
+            this.colemail.Width = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.LABnombre);
+            this.Controls.Add(this.LABcontrasena1);
+            this.Controls.Add(this.LABcontrasena2);
+            this.Controls.Add(this.LABedad);
+            this.Controls.Add(this.LABemail1);
+            this.Controls.Add(this.LABemail2);
+            this.Controls.Add(this.TBnombre);
+            this.Controls.Add(this.TBcontrasena1);
+            this.Controls.Add(this.TBcontrasena2);
+            this.Controls.Add(this.TBedad);
+            this.Controls.Add(this.TBemail1);
+            this.Controls.Add(this.TBemail2);
+            this.Controls.Add(this.BTedad);
+            this.Controls.Add(this.BTemail);
+            this.Controls.Add(this.BTaceptar);
+            this.Controls.Add(this.BTcancelar);
+            this.Controls.Add(this.BTnombre);
+            this.Controls.Add(this.BTcontrasena);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTcontrasena;
+        private System.Windows.Forms.Button BTnombre;
+        private System.Windows.Forms.Button BTcancelar;
+        private System.Windows.Forms.Button BTaceptar;
+        private System.Windows.Forms.Button BTemail;
+        private System.Windows.Forms.Button BTedad;
+        private System.Windows.Forms.TextBox TBemail2;
+        private System.Windows.Forms.TextBox TBemail1;
+        private System.Windows.Forms.TextBox TBedad;
+        private System.Windows.Forms.TextBox TBcontrasena2;
+        private System.Windows.Forms.TextBox TBcontrasena1;
+        private System.Windows.Forms.TextBox TBnombre;
+        private System.Windows.Forms.Label LABemail2;
+        private System.Windows.Forms.Label LABemail1;
+        private System.Windows.Forms.Label LABedad;
+        private System.Windows.Forms.Label LABcontrasena2;
+        private System.Windows.Forms.Label LABcontrasena1;
+        private System.Windows.Forms.Label LABnombre;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader colusuario;
+        private System.Windows.Forms.ColumnHeader colcontrasenia;
+        private System.Windows.Forms.ColumnHeader coledad;
+        private System.Windows.Forms.ColumnHeader colemail;
+    }
+}
+
---
+++ examenes1daw/201705final/final1617ej3/final1617ej3/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final1617ej3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TBcontrasena1.PasswordChar = '*';
+            TBcontrasena2.PasswordChar = '*';
+        }
+
+        int i;
+        string[] nuevafila = new string[4];
+        ListViewItem lvi;
+
+        // Validación del nombre de usuario
+        private void BTnombre_Click(object sender, EventArgs e)
+        {
+            bool ocupado = false;
+            bool sololetras = true;
+
+            if(TBnombre.Text.Length >= 4 && TBnombre.Text.Length <= 20) // el nombre tiene 4-20 caracteres
+            {
+                for(i = 0; i < TBnombre.Text.Length; i++)
+                {
+                    if (!char.IsLetter(TBnombre.Text[i]))
+                    {
+                        sololetras = false;
+                    }
+                }
+
+                if (sololetras)
+                {
+                    for(i = 0; i < listView1.Items.Count; i++)
+                    {
+                        if(listView1.Items[i].Text == TBnombre.Text)
+                        {
+                            ocupado = true;
+                        }
+                    }
+
+                    if (ocupado)
+                    {
+                        MessageBox.Show("El nombre de usuario no está disponible.");
+                        TBnombre.Focus();
+                    }
+                    else
+                    {
+                        nuevafila[0] = TBnombre.Text;
+                        TBnombre.Enabled = false;
+                        BTnombre.Enabled = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El nombre de usuario solo puede llevar letras.");
+                    TBnombre.Focus();
+                }
+            }
+            else // el nombre tiene menos de 4 o más de 20 caracteres
+            {
+                MessageBox.Show("El nombre de usuario debe tener entre 4 y 20 caracteres.");
+                TBnombre.Focus();
+            }
+        }
+
+        // Validación de la contraseña
+        private void BTcontrasena_Click(object sender, EventArgs e)
+        {
+            if(TBcontrasena1.Text.Length >= 8 && TBcontrasena1.Text.Length <= 16)
+            {
+                if(TBcontrasena1.Text == TBcontrasena2.Text)
+                {
+                    nuevafila[1] = TBcontrasena1.Text;
+                    BTcontrasena.Enabled = false;
+                    TBcontrasena1.Enabled = false;
+                    TBcontrasena2.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Las contraseñas deben coincidir.");
+                    TBcontrasena1.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("La contraseña debe tener entre 8 y 16 caracteres.");
+                TBcontrasena1.Focus();
+            }
+        }
+
+        // Validación de la edad
+        private void BTedad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int edad = Convert.ToInt32(TBedad.Text);
+
+                if(edad >= 18 && edad <= 100)
+                {
+                    nuevafila[2] = TBedad.Text;
+                    BTedad.Enabled = false;
+                    TBedad.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("La edad debe estar comprendida entre 18 y 100 años.");
+                    TBedad.Focus();
+                }
+            }
+            catch
+            {
+                throw new Exception("Solo se pueden introducir números en el campo Edad.");
+                TBedad.Focus();
+            }
+        }
+
+        // Validación del correo electrónico
+        private void BTemail_Click(object sender, EventArgs e)
+        {
+            int arrobas = 0;
+            bool caracteresprohibidos = false;
+
+            for(i = 0; i < TBemail1.Text.Length; i++)
+            {
+                if(TBemail1.Text[i] == '@')
+                {
+                    arrobas = arrobas + 1;
+                }
+                else
+                {
+                    if(TBemail1.Text[i] != '.' && !char.IsLetter(TBemail1.Text[i]) && !char.IsNumber(TBemail1.Text[i]))
+                    {
+                        caracteresprohibidos = true;
+                    }
+                }
+            }
+
+            if(arrobas == 1)
+            {
+                if (!caracteresprohibidos)
+                {
+                    if(TBemail1.Text == TBemail2.Text)
+                    {
+                        nuevafila[3] = TBemail1.Text;
+                        BTemail.Enabled = false;
+                        TBemail2.Enabled = false;
+                        TBemail1.Enabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ambas direcciones de correo electrónico deben coincidir.");
+                        TBemail1.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("La dirección solo puede contener letras, números y puntos (aparte de la arroba).");
+                    TBemail1.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("La dirección debe contener una arroba.");
+                TBemail1.Focus();
+            }
+
+            // si todos los botones de validación están deshabilitados
+            if(BTnombre.Enabled == false && BTcontrasena.Enabled == false && BTedad.Enabled == false && BTemail.Enabled == false)
+            {
+                BTaceptar.Enabled = true;
+            }
+        }
+
+        // Añade a la lista el nuevo registro con los datos proporcionados
+        private void BTaceptar_Click(object sender, EventArgs e)
+        {
+            lvi = new ListViewItem(nuevafila);
+            listView1.Items.Add(lvi);
+        }
+
+        // Devuelve el formulario a su estado inicial
+        private void BTcancelar_Click(object sender, EventArgs e)
+        {
+            TBnombre.Text = "";
+            TBcontrasena1.Text = "";
+            TBcontrasena2.Text = "";
+            TBedad.Text = "";
+            TBemail1.Text = "";
+            TBemail2.Text = "";
+            
+            TBnombre.Enabled = true;
+            BTnombre.Enabled = true;
+            BTcontrasena.Enabled = true;
+            TBcontrasena1.Enabled = true;
+            TBcontrasena2.Enabled = true;
+            BTedad.Enabled = true;
+            TBedad.Enabled = true;
+            BTemail.Enabled = true;
+            TBemail2.Enabled = true;
+            TBemail1.Enabled = true;
+
+            BTaceptar.Enabled = false;
+        }
+
+    }
+}
