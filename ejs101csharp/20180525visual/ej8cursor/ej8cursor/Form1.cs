ea04a6aba0a1f338cd6c0708668026bfa20cb3d7
Isabel <isabel.vigil.moran@gmail.com>
2018-09-14 11:17:58 +0200

    Actualizado ejercicio 8 de visual (cursor)

--- ejs101csharp/20180525visual/ej8cursor/ej8cursor/Form1.cs
+++ ejs101csharp/20180525visual/ej8cursor/ej8cursor/Form1.cs
@@ -15,51 +15,84 @@ namespace ej8cursor
         public Form1()
         {
             InitializeComponent();
+            // LBo.Location = new Point(120, 112); // le doy coordenadas iniciales
+            LBo.BackColor = Color.ForestGreen; // le doy color a la etiqueta para verla bien
         }
 
+        // Para cada botón (Norte - Sur - Este ...)
         private void BTno_Click(object sender, EventArgs e)
-        {
-            LBo.Location = new Point(12, 30);
+        { 
+            // Al pinchar el botón, muevo la etiqueta 1 punto hacia ese botón
+            LBo.Location = new Point(LBo.Location.X - 1, LBo.Location.Y - 1);
         }
 
         private void BTn_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(93, 30);
+            LBo.Location = new Point(LBo.Location.X, LBo.Location.Y - 1);
         }
 
         private void BTne_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(174, 30);
+            LBo.Location = new Point(LBo.Location.X + 1, LBo.Location.Y - 1);
         }
 
         private void BTo_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(12, 96);
+            LBo.Location = new Point(LBo.Location.X - 1, LBo.Location.Y);
         }
 
         private void BTcentro_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(93, 96);
+            // Muevo la etiqueta sobre el eje X
+
+            if (LBo.Location.X > 120) // está a la derecha
+            {
+                LBo.Location = new Point(LBo.Location.X - 1, LBo.Location.Y);
+            }
+            else
+            {
+                if(LBo.Location.X < 120) // está a la izquierda
+                {
+                    LBo.Location = new Point(LBo.Location.X + 1, LBo.Location.Y);
+                }                
+            }
+
+            // Muevo la etiqueta sobre el eje Y
+
+            if(LBo.Location.Y > 112)
+            {
+                LBo.Location = new Point(LBo.Location.X, LBo.Location.Y - 1);
+            }
+            else
+            {
+                if(LBo.Location.Y < 112)
+                {
+                    LBo.Location = new Point(LBo.Location.X, LBo.Location.Y + 1);
+                }
+            }
+
+            // Si // x == 120 && y  == 112 se queda quieta
+
         }
 
         private void BTe_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(174, 96);
+            LBo.Location = LBo.Location = new Point(LBo.Location.X + 1, LBo.Location.Y);
         }
 
         private void BTso_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(12, 163);
+            LBo.Location = LBo.Location = new Point(LBo.Location.X - 1, LBo.Location.Y + 1);
         }
 
         private void BTs_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(93, 163);
+            LBo.Location = new Point(LBo.Location.X, LBo.Location.Y + 1);
         }
 
         private void BTse_Click(object sender, EventArgs e)
         {
-            LBo.Location = new Point(174, 163);
+            LBo.Location = new Point(LBo.Location.X + 1, LBo.Location.Y + 1);
         }
     }
 }
